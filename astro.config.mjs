57b1cca0d42d2976aaea827502e2aa633ecee83a
zunisar <zzunigaenrique@gmail.com>
2026-01-13 21:26:10 -0600

    posts collection created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astroblogtest117.netlify.app/"
+  site: "https://astroblogtest117.netlify.app/",
+  integrations: [preact()]
 });
