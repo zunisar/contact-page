6b224bdb1a878bc6012e818b43e5185a55cc4607
zunisar <zzunigaenrique@gmail.com>
2026-01-13 21:33:49 -0600

    rss fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,8 @@
+import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-    const posts = await getCollection("blog");
+  const posts = await getCollection("blog");
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
